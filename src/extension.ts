babb2951dbb089b93fb84f6ae0909d7ff595fe51
jquinzell <>
2021-06-20 12:36:53 -0500

    setup command for status bar item

--- src/extension.ts
+++ src/extension.ts
@@ -58,18 +58,20 @@ function getThemeConfig() {
 }
 
 export function activate(context: vscode.ExtensionContext) {
+  const randomizeCommandId = "theme-gallery.randomize"
+  context.subscriptions.push(
+    vscode.commands.registerCommand(randomizeCommandId, () => {
+      changeTheme()
+    })
+  )
   statusBarItem = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left
   )
   statusBarItem.text = userSettings.get<string>("workbench.colorTheme") || ""
+  statusBarItem.command = randomizeCommandId
   statusBarItem.show()
 
   context.subscriptions.push(statusBarItem)
-  context.subscriptions.push(
-    vscode.commands.registerCommand("theme-gallery.randomize", () => {
-      changeTheme()
-    })
-  )
   context.subscriptions.push(
     vscode.workspace.onDidChangeConfiguration((e) => {
       if (e.affectsConfiguration("themeGallery")) {
